ac7b4b1534cfc47623d7c0217fecde771256ff00
Richard Chang <rchang9999@yahoo.com>
2026-05-15 13:37:12 -0400

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    public Stack<Integer> stack;
+    public Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        int min;
+        if(minStack.isEmpty()){
+            min = val;
+        }else{
+            min = Math.min(val,minStack.peek());
+        }
+        minStack.push(min);
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
