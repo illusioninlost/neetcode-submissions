e2e0477f0ce40df1bd71c3dd0b4c651d851e6102
Richard Chang <rchang9999@yahoo.com>
2026-05-21 19:34:23 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(Character c: s.toCharArray()){
+
+            if(c == '(' || c=='{' || c=='['){
+                stack.push(c);
+            }else if(c ==')'){
+                if(stack.isEmpty() || stack.pop() != '('){
+                    return false;
+                }
+                continue;
+            }else if(c ==']'){
+                if(stack.isEmpty() || stack.pop() != '['){
+                    return false;
+                }
+                continue;
+            }else if(c =='}'){
+                if(stack.isEmpty() || stack.pop() != '{'){
+                    return false;
+                }
+                continue;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
